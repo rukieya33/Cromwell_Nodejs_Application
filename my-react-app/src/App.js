de0e6caf7597efa375a8a21bf86185237d5e31fe
Rukieya Ainoo <a012702h@student.staffs.ac.uk>
2024-11-21 08:29:29 +0000

    new component files added to react app

---
+++ my-react-app/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import {useState} from 'react'
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import Register from './Register/Register.js';
+import Home from './Home/Home.js';
+import Login from './Login/Login.js';
+import LandingPage from './LandingPage/LandingPage.js';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+          <h2 id="appTitle">Full stack Nodejs App (Frontend) </h2>
+          <BrowserRouter>
+          <Routes>
+              <Route path="home" element={<Home />} />
+              <Route path="landing-page" element={<LandingPage />} />
+              <Route path="register" element={<Register />} />
+              <Route path="login" element={<Login />} />
+          </Routes>
+
+          <ul>
+          <li><Link to="/home" }><h2 id="homeText">Home</h2></Link></li>
+           <li><Link to="/landing-page" }><h2 id="landingPageText" >Landing Page</h2></Link></li>
+          <li><Link to="/register" }><h2 id="registerText">Register</h2></Link></li>
+          <li><Link to="/login"  }><h2 id="loginText">Login</h2></Link></li>
+          </ul>
+
+         </BrowserRouter>
+      </header>
+      <footer>
+      &copy;
+      </footer>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ my-react-app/src/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+//import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+        <h1>Welcome to Landing Page </h1>
+    </div>
+  );
+}
+
+export default LandingPage;
